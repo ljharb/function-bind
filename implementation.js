42820c525b5534a422632aae6024d8f9f3388ecf
xguest <xguest@list.ru>
2016-07-03 00:10:24 +0500

    Update arguments counter of function 'bound'

--- implementation.js
+++ implementation.js
@@ -11,49 +11,25 @@ module.exports = function bind(that) {
     var args = slice.call(arguments, 1);
 
     var bound;
-/*
-    var binder = function () {
-        if (this instanceof bound) {                 // is constructor bound
-            var result = target.apply(               // get result value
-                this,                                // this scope
-                args.concat(slice.call(arguments))   // get arguments
-            );
-            if (Object(result) === result) {         // if result is not a primitive
-                return result;
-            }
-            return this;                             // or this
-        } else {
-            return target.apply(                     // get value
-                that,                                // another scope
-                args.concat(slice.call(arguments))   // get arguments
-            );
-        }
-    };
-
-    var binder = function () {                       // equivalently
-        var result;                                  // for a primitive always this scope
-        if (instanceof bound) result = this;         // if constructor bound this scope
-        else result = that;                          // another scope
-        return target.apply(                         // get value
-            result,
-            args.concat(slice.call(arguments))       // get arguments
-        )
-    }
-*/
     var binder = function () {                       // ternary
         return target.apply(                         // get value
             this instanceof bound ? this : that,     // if constructor bound this scope or other
             args.concat(slice.call(arguments))       // get arguments
         );
     };
-
-    var boundLength = Math.max(0, target.length - args.length);
-    var boundArgs = [];
+/*
+    var boundLength = Math.max(0, target.length - args.length); // function length
+    var boundArgs = [];                              // array of arguments counter
     for (var i = 0; i < boundLength; i++) {
-        boundArgs.push('$' + i);
+        boundArgs.push('$' + i);                     // build string
     }
 
     bound = Function('binder', 'return function (' + boundArgs.join(',') + '){ return binder.apply(this,arguments); }')(binder);
+*/
+    // we do not use separate arguments, we need only the number of
+    boundArgs = Array(Math.max(0, 1 + target.length - args.length)).join('$').split('');
+    // converting an array to string by default separator comma
+    bound = Function('binder', 'return function (' + boundArgs + '){ return binder.apply(this,arguments); }')(binder);
 
     if (target.prototype) {
         var Empty = function Empty() {};
